e5b8a318d82b6e05f148a718e3736d97da28ff74
nanangqq <gingggg@gmail.com>
2021-12-01 18:05:21 +0900

    changed pragma version

--- client/src/memo.ts
+++ client/src/memo.ts
@@ -1,2 +1,1 @@
-// export const address = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0' // local
-export const address = '0x180b562341114C6af9Aac3E502AD4935b19dE348' // ropsten
+export const address='0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
